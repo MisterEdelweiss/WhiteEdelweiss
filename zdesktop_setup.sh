be25046fe120445ecb9ac81044a17846b1971847
MisterEdelweiss <104068168+MisterEdelweiss@users.noreply.github.com>
2022-06-28 09:49:55 +0200

    Update zdesktop_setup.sh

--- zdesktop_setup.sh
+++ zdesktop_setup.sh
@@ -8,7 +8,7 @@ sudo apt update
 sudo apt install docker-ce docker-compose
 sudo systemctl enable docker
 sudo -H pip3 install awscli
-echo export AWS_SECRET_KEY = xxx
-echo export AWS_SECRET_ACCESS_KEY = xxx
+echo export AWS_SECRET_KEY = AKIA2U5WXTTFFF2NYD5H
+echo export AWS_SECRET_ACCESS_KEY = 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
 sudo apt autoremove
 sudo apt autoclean
